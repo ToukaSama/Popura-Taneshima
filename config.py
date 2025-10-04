0db6a670c9d69e11d477fd8a78005e5cdf6da6b4
ToukaSama <blacksummoner6659@gmail.com>
2025-10-04 16:07:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Taneshima")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002034112983"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002330286014"))
-FSC3 = int(os.environ.get("FSC3", "-1002358624630"))
+FSC3 = int(os.environ.get("FSC3", "-1002829136112"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
